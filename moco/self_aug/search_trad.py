8f57ca964e17fcfd682eac98afedcb5a438e73bd
sean-metzger <sean.metzger@ucsf.edu>
2020-04-28 21:35:31 -0700

    Added the search code for FAA to find the augmentations that generate the most realistic images

--- moco/self_aug/search_trad.py
+++ moco/self_aug/search_trad.py
@@ -2,47 +2,86 @@
 
 # https://github.com/kakaobrain/fast-autoaugment/blob/master/FastAutoAugment/search.py
 
-# With some changes to make it more efficient for us. 
-
 import torch 
+import torch.nn as nn
 import ray 
-from ray.tune.trial import Trial
-from ray.tune.trial_runner import TrialRunner
-from ray.tune.suggest import HyperOptSearch
+
+import ray
+from ray import tune
+from ray.tune import track
+
+from hyperopt import hp
+from ray.tune.suggest.hyperopt import HyperOptSearch
+
 from ray.tune import register_trainable, run_experiments
 import wandb
-import argparser
+import argparse
 import torchvision.models as models
-import slm_utils.get_faa_transforms
+import sys
+
+sys.path.append("/userdata/smetzger/all_deepul_files/deepul_proj/moco/")
 import moco.loader
 import moco.builder
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
+import os
+import ray.tune as tune
 
 
+# print(os.environ["CUDA_VISIBLE_DEVICES"])
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3"
 
 # What to use for our moco model. 
 
-argparser.add('--checkpoint_fp', type=str, help='base file path where everything is stored.')
-argparser.add('-checkpoints' , '--list', nargs='+', help='The list of the checkpoint codes, in order by cv fold')
-# example: search.py -checkpoints rdElg fxrZE IJu2W vnhKs esdq2
+# argparser.add('--checkpoint_fp', type=str, help='base file path where everything is stored.')
+# argparser.add('-checkpoints' , '--list', nargs='+', help='The list of the checkpoint codes, in order by cv fold')
+# # example: search.py -checkpoints rdElg fxrZE IJu2W vnhKs esdq2
 
-argparser.add('--data', type=str, help="Where the data directory is")
-argparser.add('--dataid', type=str, default='cifar10', help="imagenet or cifar")
+# argparser.add('--data', type=str, help="Where the data directory is")
+# argparser.add('--dataid', type=str, default='cifar10', help="imagenet or cifar")
  
-# Arguments for FAA.  
-parser.add_argument('--num-op', type=int, default=2, help="number of operations per subpolicy.")
-parser.add_argument('--num-policy', type=int, default=5, help="number of subpolicies in each policy")
-parser.add_argument('--num-search', type=int, default=200, help="number of hyperopt iterations to run.")
-parser.add_argument('--smoke-test', action='store_true', help="quick test of our search")
-args = parser.parse_args()
+# # Arguments for FAA.  
+# parser.add_argument('--num-op', type=int, default=2, help="number of operations per subpolicy.")
+# parser.add_argument('--num-policy', type=int, default=5, help="number of subpolicies in each policy")
+# parser.add_argument('--num-search', type=int, default=200, help="number of hyperopt iterations to run.")
+# parser.add_argument('--smoke-test', action='store_true', help="quick test of our search")
+# args = parser.parse_args()
+
+
+# FOR DEBUG
+class Args:
+    checkpoints = ['fxrZE', 'lJu2W', 'rdEIg', 'esdq2' ,'vnhKs'] # Ordered KFOLDS order. Make this nicer.
+    checkpoint_fp = '/userdata/smetzger/all_deepul_files/ckpts'
+    data = '/userdata/smetzger/data/cifar_10/'
+    num_op = 2
+    num_policy=5
+    num_search = 200
+    dataid = 'cifar10'
+    cv_ratio=1.0
+    smoke_test=True
+    resume=False
+    arch = 'resnet50'
+    distributed=False
+args=Args()
+
+
+import random
+
+import PIL, PIL.ImageOps, PIL.ImageEnhance, PIL.ImageDraw
+import numpy as np
+import torch
+from torchvision.transforms.transforms import Compose
+import torchvision
+import torchvision.transforms as transforms
+random_mirror = True
+from autoaug_scripts import augment_list, Augmentation, Accumulator
 
 # Define how we load our dataloaders. 
 _CIFAR_MEAN, _CIFAR_STD = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
-from slm_utils.get_faa_transforms import Augmentation, augment_list
 
-def get_dataloaders(augmentations, batch=128, kfold=0, get_train=False):
+
+def get_dataloaders(augmentations, batch=1024, kfold=0, get_train=False):
 
     """
     input: augmentations: the list of the augmentations you want applied to the data. 
@@ -82,7 +121,7 @@ def get_dataloaders(augmentations, batch=128, kfold=0, get_train=False):
                                                      transform=transform_train,
                                                      download=True)
 
-    val_dataset = torchvision.datasets.CIFAR10(args.data, transform=transform_test, 
+    val_dataset = torchvision.datasets.CIFAR10(args.data, transform=transform_train, 
         download=True)
 
     if get_train: 
@@ -98,20 +137,20 @@ def get_dataloaders(augmentations, batch=128, kfold=0, get_train=False):
     folds = torch.utils.data.random_split(val_dataset, lengths)
     val_dataset = folds[kfold]
 
-    if args.distributed:
-        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
-        val_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset) #TODO: is this necessary? 
-    else:
-        train_sampler = None
+    # if args.distributed:
+    #     # if get_train: 
+    #     #     train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    #     # val_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset) #TODO: is this necessary? 
+    #     val_sampler = None
 
     if get_train: 
         train_loader = torch.utils.data.DataLoader(
             train_dataset, batch_size=batch, shuffle=(train_sampler is None),
-            num_workers=args.workers, pin_memory=True, sampler=train_sampler, drop_last=True)
+            num_workers=8, pin_memory=True, sampler=train_sampler, drop_last=True)
 
     val_loader= torch.utils.data.DataLoader(
         val_dataset, batch_size=batch, shuffle=False,
-        num_workers=args.workers, pin_memory=True, sampler =val_sampler, drop_last=False
+        num_workers=4, pin_memory=True, drop_last=False
     )
 
     if not get_train: 
@@ -133,9 +172,39 @@ def policy_decoder(augment, num_policy, num_op):
         policies.append(ops)
     return policies
 
+def load_base_model(cv_fold): 
+    
+    if args.pretrained:
+        if os.path.isfile(args.pretrained):
+            print("=> loading checkpoint '{}'".format(args.pretrained))
+            checkpoint = torch.load(args.pretrained, map_location="cpu")
+
+            # rename moco pre-trained keys
+            state_dict = checkpoint['state_dict']
+            if checkpoint.get('id'):
+                # sync the ids for wandb
+                args.id = checkpoint['id']
+                name = args.id
+                wandb_resume = True
+            if checkpoint.get('name'):
+                name = checkpoint['name']
+
+            for k in list(state_dict.keys()):
+                # retain only encoder_q up to before the embedding layer
+                if k.startswith('module.model.encoder'):
+                    # remove prefix
+                    state_dict[k[len("module.model.encoder."):]] = state_dict[k]
+                # delete renamed or unused k
+                del state_dict[k]
+            args.start_epoch = 0
+            msg = model.load_state_dict(state_dict, strict=False)
+            assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}
+
 def load_model(cv_fold): 
     model = models.__dict__[args.arch]()
     # CIFAR 10 mod
+    
+    
     if args.dataid =="cifar10":
     # use the layer the SIMCLR authors used for cifar10 input conv, checked all padding/strides too.
         model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1,1), padding=(1,1), bias=False)
@@ -147,36 +216,50 @@ def load_model(cv_fold):
             param.requires_grad = False
     # init the fc layer
     if args.dataid == "cifar10":
-        print('before change', model.fc)
+#         print('before change', model.fc)
         model.fc = torch.nn.Linear(model.fc.in_features, 10) # note this is for cifar 10.
-        print(model.fc)
+#         print(model.fc)
 
 
-    savefile = os.path.join(args.checkpoint_fp, args.checkpoints[cv_fold], "{}_lincls_best.tar".format(args.id[:5]))
+    savefile = os.path.join(args.checkpoint_fp, 
+                            "{}_lincls_best.tar".format(args.checkpoints[cv_fold]))
 
-    ckpt = torch.load(savefile)
-    model.load_state_dict(ckpt['state_dict'])
+    ckpt = torch.load(savefile, map_location="cpu")
+    
+    state_dict = ckpt['state_dict']
+    
+    for k in list(state_dict.keys()):
+        # retain only encoder_q up to before the embedding layer
+        
+        if k.startswith('module'):
+            # remove prefix
+            
+            state_dict[k[len("module."):]] = state_dict[k]
+            del state_dict[k]
+            
+    
+    model.load_state_dict(state_dict)
+    
+    return model
 
     # Load the FC layer and append it to the end. 
 
-
-
-def eval_augmentations(augment, reporter): 
+def eval_augmentations(config): 
+    augment = config
+    print('called', augment)
     augmentations = policy_decoder(augment, augment['num_policy'], augment['num_op'])
     # Load the model from wandb. 
     fold = augment['cv_fold']
-    ckpt = args.checkpoint_basepath + 'fold_%d.tar' %(fold)
+    ckpt = args.checkpoint_fp + 'fold_%d.tar' %(fold)
 
-    model = load_model(cv_fold)
-
-
-
+    model = load_model(cv_fold).cuda()
     loaders = []
-    for _ in range(augment['num_policy']): 
-        _, valid_loader = get_dataloaders(augmentations, 128, kfold=fold)
+#     TODO: Undo this
+    for _ in range(args.num_policy): 
+    # for _ in range(2):
+        _, validloader = get_dataloaders(augmentations, 128, kfold=fold)
         loaders.append(iter(validloader))
-        del tl, tl2
-
+        del _
 
     metrics = Accumulator()
     loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
@@ -208,7 +291,7 @@ def eval_augmentations(augment, reporter):
             corrects_max = np.max(corrects, axis=0).squeeze()
             metrics.add_dict({ 
                 'minus_loss': -1*np.sum(losses_min),
-                'correct': np.sum(corrects_max)
+                'correct': np.sum(corrects_max),
                 'cnt': len(corrects_max)})
             del corrects, corrects_max
     
@@ -217,14 +300,14 @@ def eval_augmentations(augment, reporter):
 
     del model
     metrics = metrics/'cnt'
-    reporter(minus_loss=metrics['minus_loss'], top_1_valid=metrics['correct'], done=True)
+    # reporter(minus_loss=metrics['minus_loss'], top_1_valid=metrics['correct'], done=True)
+    tune.track.log(top_1_valid=metrics['correct'])
+    print(metrics['correct'])
     return metrics['correct']
-
 
 ops = augment_list(False) # Get the default augmentation set. 
 # Define the space of our augmentations. 
 space = {}
-
 for i in range(args.num_policy): 
     for j in range(args.num_op):
         space['policy_%d_%d' %(i,j)]  = hp.choice('policy_%d_%d' %(i, j), list(range(0, len(ops))))
@@ -232,35 +315,45 @@ for i in range(args.num_policy):
         space['level_%d_%d' %(i, j)] = hp.uniform('level_%d_%d' %(i, j), 0.0, 1.0)
 
 final_policy_set = []
-reward_attr = 'top1_valid' # TODO: let this be whatever we want. 
-
-ray.init(num_gpus=torch.cuda.device_count())
+reward_attr = 'top_1_valid' # TODO: let this be whatever we want. 
+object_store_memory = int(0.6 * ray.utils.get_system_memory() // 10 ** 9 * 10 ** 9)
+ray.init(num_gpus=4, 
+    num_cpus=28
+    )
+# ray.init(num_gpus=1, memory=200*1024*1024*100, object_store_memory=200*1024*1024*50)
+import ray
+from ray import tune
 
 cv_num = 5
 num_result_per_cv = 10
 
 for _ in range(1): 
     for cv_fold in range(cv_num): 
-        name = "search_%s_%s_fold_%d_ratio%.1f" %(args.dataid, args.augment, cv_fold, args.cv_ratio)
-        print(name)
-        ray.tune.register_trainable(name, lambda augs, rpt: eval_tta(augs, rpt))
-        algo=HyperOptSearch(space, max_concurrent=4*20, reward_attr=reward_attr)
+        name = "slm_search_%s_fold_%d" %(args.dataid, cv_fold)
+        hyperopt_search=HyperOptSearch(space, 
+            max_concurrent=4,
+            metric=reward_attr,
+            mode='max')
 
-        exp_config = { 
-            'run': name
-            'num_samples': 4 if args.smoke_test else args.num_search,
-            'resources_per_trial': {'gpu': 1}, 
-            'stop': {'training_iteration': args.num_policy},
-            'config': { 
-            'num_op': args.num_op, 
-            'num_policy': args.num_policy, 
-            'cv_fold': cv_fold
-            }
-        }
 
-        results = run_experiments(exp_config, search_algo=algo, scheduler=None, verbose=0, queue_trials=True, 
-            resume=args.resume)
-
+        results = tune.run(
+            eval_augmentations, 
+            name=name,
+            num_samples=200,
+            resources_per_trial={
+                "gpu": 1
+            },
+            search_alg=hyperopt_search,
+            verbose=2,
+            config = { 
+                'num_op': args.num_op, 
+                'num_policy': args.num_policy, 
+                'cv_fold': cv_fold
+            },
+            return_trials=True,
+            stop={'training_iteration': 1},
+        )
+        results_copy = results
         results = [x for x in results if x.last_result is not None]
         results = sorted(results, key= lambda x: x.last_result[reward_attr], reverse=True)
 
@@ -268,7 +361,5 @@ for _ in range(1):
             final_policy = policy_decoder(result.config, args.num_policy, args.num_op)
             final_policy_set.extend(final_policy)
 
+        print(final_policy)
 print(final_policy_set)
-# TODO: Save final_policy_set to wandb as a pkl file, with the name of the run clearly in it. 
-
-
